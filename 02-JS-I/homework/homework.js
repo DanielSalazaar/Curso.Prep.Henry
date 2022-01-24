cd2422f961901c2e423410a831ddcbe3805ebe66
daniel01 <danielsalazar297@gmail.com>
2022-01-24 12:44:26 -0600

    haciendo mas cambios en linea 4

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = 'just make a change';
+const nuevaString = 'just make a change, and now just make another change';
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
